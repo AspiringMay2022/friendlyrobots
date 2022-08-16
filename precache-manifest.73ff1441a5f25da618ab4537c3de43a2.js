725cce73f40c072dbf2bfec43087dc35d5ea30bd
Duduy <beauty.contis@yahoo.com>
2022-08-16 16:34:53 -0600

    Updates

---
+++ precache-manifest.73ff1441a5f25da618ab4537c3de43a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5592c7eebbb094853fba",
+    "url": "/friendlyrobots/static/css/main.0a9bcc48.chunk.css"
+  },
+  {
+    "revision": "5592c7eebbb094853fba",
+    "url": "/friendlyrobots/static/js/main.5592c7ee.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/friendlyrobots/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4419bfc4db82a3bd3518",
+    "url": "/friendlyrobots/static/css/2.fe8b022b.chunk.css"
+  },
+  {
+    "revision": "4419bfc4db82a3bd3518",
+    "url": "/friendlyrobots/static/js/2.4419bfc4.chunk.js"
+  },
+  {
+    "revision": "f3f41c701d3a1e139875",
+    "url": "/friendlyrobots/static/js/runtime~main.f3f41c70.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendlyrobots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "567cd2eb039d00bc7be4fa8bad036220",
+    "url": "/friendlyrobots/index.html"
+  }
+];
